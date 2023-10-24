2e4849caf33c46f512b9a4b8e3898d3ed33f3b8f
Stefan Espant <stefan.van.der.kort@hva.nl>
2023-10-24 21:24:28 +0200

    v0.0.2
    custom menu navigation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,11 @@
 <Header />
 
 <main>
+    <section>
+        <h1>Welkom in Eva's kookboek</h1>
+        <h2>Alle recepten en inspiratie</h2>
+        
+    </section>
 
 <!-- {#each data.recepts as recept}
     <p>{recept.bereiding}</p>
